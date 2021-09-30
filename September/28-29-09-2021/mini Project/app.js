b21f6d5abba26cee8eeb9f860e67e2320c499892
kishan-WCG <kishan.l@webcodegenie.com>
2021-09-30 18:46:40 +0530

    Admin panel work in process

---
+++ September/28-29-09-2021/mini Project/app.js
@@ -0,0 +1,59 @@
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var session = require('express-session');
+var expressfileupload = require('express-fileupload');
+
+var app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+
+mongoose
+    .connect("mongodb://login:login@localhost:27017/login")
+    .then(() => console.log("Connection Open"))
+    .catch((error) => console.log(error))
+
+// Session APP.USE
+app.use(session({
+    secret: 'keyboard cat',
+    cookie: {
+        maxAge: 6000000
+    }
+}))
+app.use(expressfileupload());
+// app.use(expressfileupload);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
+});
+
+module.exports = app;
